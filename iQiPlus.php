9b2ea29624744fa542e4ddaae57050578a9890a2
iQI <iQI@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-28 12:45:49 +0000

    First version
    git-svn-id: https://plugins.svn.wordpress.org/iqiplus-sns-share-tools-for-japan/trunk@904076 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ iQiPlus.php
@@ -0,0 +1,38 @@
+<?php
+    
+    /*
+     Plugin Name: iQiPlus
+     Version:     1.0
+     Author:      iQi
+     Author URI:  http://wiki.jp.ai/at/iQiPlus
+     Plugin URI:  http://plus.jp.net.ai/
+     Description: SNS share tools for japan.
+     */
+    
+    
+    // for add to content ...
+    function iQiPlus($HTM){
+        
+        if(!is_single()): return $HTM; endif;
+        
+        global $post;
+        //wp_enqueue_style('iQiPlus', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/iQiPlus.css', NULL, '1.0');
+        wp_enqueue_style('iQiPlus',plugins_url( 'iQiPlus.css', __FILE__ ), NULL, '1.0');
+        
+        
+        //$STYLE = $post['iqiplusstyle']?$_POST['iqiplusstyle']:'original';
+        //$ALIGN = $post['iqiplusalign']?$_POST['iqiplusalign']:'left';
+        //$TOTOP = $post['iqiplustotop']?$_POST['iqiplustotop']:'on';
+        $STYLE = 'simple';
+        $ALIGN = 'left';
+        $TOTOP = 'on';
+        
+        $HTM .= '<div id="iQiPlus" class="clearfix">'."\n";
+        $HTM .= '<b><i>iQiPlus</i></b>'."\n";
+        $HTM .= '<script>iQiPlusStyle="'.$STYLE.'";iQiPlusAlign="'.$ALIGN.'";iQiPlusTotop="'.$TOTOP.'";</script><script src="http://api.jp.net.ai/iqi.plus/iqi.plus.wp.js" type="text/javascript"></script>';
+        $HTM .= '<br clear="all"></div><!-- #iQiPlus -->';
+        
+        return $HTM;
+    }
+    
+    add_filter('the_content', 'iQiPlus');
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Plugin Name ===
+
+Plugin Name:       iQiPlus
+Contributors:      iQi
+Requires at least: 1.0.0
+Tested up to:      1.0.0
+Stable tag:        trunk
+Help link:         http://wiki.jp.ai/at/iQiPlus
+Site Link:         http://plus.jp.net.ai/
+Tags:              iQiPlus,SNS, Japan, Share, Button,sharethis,addthis,まとめボタン,ツイートボタン,いいねボタン,はてな,Googleプラス
+License:           GPLv2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+SNS share tools for japan(日本人のためにデザインしたソーシャルシェアツールです。)
+
+
+== Description ==
+
+SNS share tools for japan(日本人のためにデザインしたソーシャルシェアツールです。)
+ソースコードをコピーして、あなたのWebサイトの表示したい場所に張り付けるだけで、無料、手間なし、カンタンでiQiPlusまとめボタンを設置。（ツイートボタン、いいねボタン、はてな、Googleプラスなどのボタンをまとめて簡単設置）
+
+
+== Installation ==
+
+wordpress pluginsからiQiPlusを検索して自動的にインストールできます。また、削除することも簡単です。
+
+
+== Frequently Asked Questions ==
+
+なし
+
+== Changelog ==
+
+
+= 2014-04-16 1.0.0 =
+正式リリース ^_^
+
+== Upgrade Notice ==
+
+なし
+
+== Screenshots ==
+
+1. iQiPlus for WordPress
+
